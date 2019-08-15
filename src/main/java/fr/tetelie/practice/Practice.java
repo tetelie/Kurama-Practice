baddd6c0f0329f9ba43c0a30dc599d12b900ad32
tetelie <elie.deletang.legravier@gmail.com>
2019-08-15 23:35:49 +0200

    Setup HikariCP and add some dependencies

---
+++ src/main/java/fr/tetelie/practice/Practice.java
@@ -0,0 +1,58 @@
+package fr.tetelie.practice;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Practice extends JavaPlugin {
+
+    private static @Getter Practice instance;
+
+    public Connection connection;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveResource("hikari.properties", false);
+        registerEvent();
+        registerCommand();
+        setupHikariCP();
+    }
+
+    @Override
+    public void onLoad() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    private void registerCommand() {
+
+    }
+
+    private void registerEvent() {
+
+    }
+
+    private void setupHikariCP()
+    {
+        String configPath = getDataFolder()+"\\hikari.properties";
+        try {
+            HikariConfig config = new HikariConfig(configPath);
+            HikariDataSource ds = new HikariDataSource(config);
+            connection = ds.getConnection();
+        } catch (SQLException e)
+        {
+            System.out.println("[Practice] Error could not connect to SQL database.");
+            e.printStackTrace();
+        }
+        System.out.println("[Practice] Successfully connected to the SQL database.");
+    }
+
+
+}
